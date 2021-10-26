8ab15281411a092bcfbfcd3dfa72ff9af8bb4667
Sagal Ghelle <sagal.ghelle@gmail.com>
2021-10-25 21:08:50 -0400

    updating function

--- head.js
+++ head.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    return `🔴🔴🔴 Assertion Passed: ${actual} === ${expected}`;
+    return `✅✅✅  Assertion Passed: ${actual} === ${expected}`;
   } else {
-    return `✅✅✅ Assertion Failed: ${actual} !== ${expected} `;
+    return `🔴🔴🔴 Assertion Failed: ${actual} !== ${expected} `;
   }
 };
 const head = function (arr){
@@ -11,6 +11,4 @@ return arr[0]
 
 console.log(assertEqual(head([5,6,7]), 5));
 console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
-// TEST CODE
-console.log(assertEqual("Shoes", "Pants"));
-console.log(assertEqual(1, 1));
+console.log(assertEqual(head([1]), 2))
