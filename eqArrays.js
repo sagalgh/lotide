bb3a447495ae89c5e1b5dced53159045c779a5de
Sagal Ghelle <sagal.ghelle@gmail.com>
2021-10-26 21:41:18 -0400

    updated code

--- eqArrays.js
+++ eqArrays.js
@@ -16,8 +16,8 @@ const eqArrays = function (arr1, arr2) {
     return false;
   }
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] == arr2[i]) {
-    return true;
+    if (arr1[i] !== arr2[i]) {
+    return false;
     }
   }
   return true
