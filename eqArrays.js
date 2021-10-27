37565f9b7654be00f85dd446d6aca14b6f49cd00
Sagal Ghelle <sagal.ghelle@gmail.com>
2021-10-26 21:39:12 -0400

    minor changes

--- eqArrays.js
+++ eqArrays.js
@@ -7,18 +7,17 @@ const assertEqual = function (actual, expected) {
 };
 
 // TEST CODE
-console.log(assertEqual([1, 2, 3], [1, 2, 3]));
 //console.log(assertEqual(1, 1));
 
 /* Implement a function eqArrays which takes in two arrays 
 and returns true or false, based on a perfect match. */
 const eqArrays = function (arr1, arr2) {
   if (arr1.length !== arr2.length) {
-    console.log("fail")
+    return false;
   }
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] == arr2[i]) {
-      console.log("pass")
+    return true;
     }
   }
   return true
@@ -26,5 +25,5 @@ const eqArrays = function (arr1, arr2) {
 
 
 
-console.log(eqArrays([1, 2], [1, 2, 3]))
+console.log(`new:`+ eqArrays([1, 2], [1, 2, 3]))
 console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)); // => should PASS
