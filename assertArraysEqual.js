0dccb86ca795d03bd11f97061d486804f87ffe29
Sagal Ghelle <sagal.ghelle@gmail.com>
2021-11-01 16:15:52 -0400

    refractoring

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,4 @@
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-    return false;
-    }
-  }
-  return true
-}
-
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function (arr1, arr2){
   const result = eqArrays(arr1,arr2)
@@ -20,7 +9,6 @@ const assertArraysEqual = function (arr1, arr2){
   console.log (`🔴🔴🔴 Assertion Failed: ${arr1} !== ${arr2}`);
   }
   }
-  assertArraysEqual([1,2,3], [1,2,3,4])
 
-
+module.exports = assertArraysEqual;
   
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,2 @@
+const  assertArraysEqual= require('../assertArraysEqual');
+console.log(assertArraysEqual([1,2,4], [1,2,3,4])  , true)
